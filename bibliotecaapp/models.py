56fd982f886da54ab556298a87ffc5124b1f7597
Rubencito2002 <ruben.machado1920@gmail.com>
2023-11-23 12:57:15 +0100

    update models.py

--- bibliotecaapp/models.py
+++ bibliotecaapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class Usuario(AbstractUser):
     dni = models.CharField(max_length=20)
     direccion = models.CharField(max_length=255)
