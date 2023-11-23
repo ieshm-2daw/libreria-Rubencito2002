8e0b1649a5b7fe613bab46996ec9bd8acf1dcc7c
Rubencito2002 <ruben.machado1920@gmail.com>
2023-11-23 12:34:54 +0100

    Comienzo del Proyecto

---
+++ bibliotecaapp/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class Usuario(AbstractUser):
+    dni = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=20)
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    bibliografia = models.TextField()
+    foto = models.ImageField()
+
+class Editorial(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=255)
+    web = models.URLField()
+
+class Libro(models.Model):
+    DISPONIBILIDA = [
+        ('disponible', 'Disponible'),
+        ('prestado', 'Prestado'),
+        ('en_proceso', 'En proceso de préstamo'),
+    ]
+
+    titulo = models.CharField(max_length=255)
+    autores = models.ManyToManyField(Autor)
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
+    fecha_publicacion = models.DateField()
+    genero = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=13, unique=True)
+    resumen = models.TextField()
+    disponibilidad = models.CharField(max_length=20, choices=DISPONIBILIDA, default='disponible')
+    portada = models.ImageField()
+
+class Prestamos(models.Model):
+    ESTADO = [
+        
+    ]
+
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    fecha_prestamo = models.DateField()
+    fecha_devolucion = models.DateField(null=True, blank=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    estado = models.CharField(max_length=20, choices=ESTADO, default='prestado')
